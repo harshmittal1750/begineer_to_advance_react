9e298f39563c1a3bf726819532efb03f4d3e8a7b
HARSH MITTAL <harshmittal1750@gmail.com>
2020-10-30 15:38:13 +0530

    components-1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar,NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents';
 import './App.css';
+ 
 
 
 function App() {
   return (
-    <div className="App">
+    <div >
    
      <Navbar dark color="primary">
        <div className="container"></div>
        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
      </Navbar>
-  
+  <Menu/>
     </div>
   );
 }
